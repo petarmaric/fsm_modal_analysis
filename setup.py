cac4f68d372fcc1c15cc8d8f60a1d180f975fe3f
Petar Marić <petar.maric@gmail.com>
2018-01-29 23:07:19 +0100

    Initial shot at creating a distutils/pip compatible setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='fsm_modal_analysis',
+    version='1.0.0',
+    url='https://bitbucket.org/petar/fsm_modal_analysis',
+    license='BSD',
+    author='Petar Maric',
+    author_email='petarmaric@uns.ac.rs',
+    description='Console app and Python API for visualization and modal '\
+                'analysis of the parametric model of buckling and free '\
+                'vibration in prismatic shell structures, as computed by the '\
+                'fsm_eigenvalue project.',
+    long_description=open('README').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+    platforms='any',
+    py_modules=['physical_dualism'],
+    entry_points={
+        'console_scripts': ['fsm_modal_analysis=fsm_modal_analysis:main'],
+    },
+    install_requires=open('requirements.txt').read().splitlines(),
+)
