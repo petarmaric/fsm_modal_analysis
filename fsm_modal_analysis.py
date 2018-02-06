2058e45c5a7bcf5d878645d0cac4f3e3ed631bb6
Petar Mari? <petar.maric@gmail.com>
2018-02-06 14:18:07 +0100

    Decoupled the `load_modal_composites` function to https://bitbucket.org/petar/fsm_load_modal_composites for reusability

--- fsm_modal_analysis.py
+++ fsm_modal_analysis.py
@@ -6,13 +6,11 @@ from timeit import default_timer as timer
 import matplotlib
 matplotlib.use('Agg') # Fixes weird segfaults, see http://matplotlib.org/faq/howto_faq.html#matplotlib-in-a-web-application-server
 
-from dynamic_pytables_where_condition import read_from_table
+from fsm_load_modal_composites import load_modal_composites
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-import tables as tb
-import yaml
 
 
 __version__ = '1.0.1'
@@ -84,19 +82,6 @@ def plot_modal_composite(base_key, modal_composites, column_units, column_descri
         plt.ylabel(_get_column_title('t_b'))
 
     logging.info("Plotting completed in %f second(s)", timer() - start)
-
-def load_modal_composites(results_file, **filters):
-    logging.info("Loading modal composites from '%s'...", results_file)
-    start = timer()
-    with tb.open_file(results_file) as fp:
-        table = fp.root.parameter_sweep.modal_composites
-        modal_composites = read_from_table(table, **filters)
-
-        column_units = yaml.load(table.attrs.column_units_as_yaml)
-        column_descriptions = yaml.load(table.attrs.column_descriptions_as_yaml)
-
-        logging.info("Loading completed in %f second(s)", timer() - start)
-        return modal_composites, column_units, column_descriptions
 
 def configure_matplotlib(cmap=DEFAULT_CMAP):
     matplotlib.rc('figure',
